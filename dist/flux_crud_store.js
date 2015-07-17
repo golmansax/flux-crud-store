4b4aafc98626131c346453507b68e89e72149595
Holman Gao <holman@golmansax.com>
2015-07-16 17:58:52 -0700

    Build new package

--- dist/flux_crud_store.js
+++ dist/flux_crud_store.js
@@ -13,6 +13,145 @@
 require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
+var extend = require('backbone').Collection.extend;
+var _ = require('underscore');
+
+var CrudStoreActions = function () {
+};
+CrudStoreActions.extend = extend;
+CrudStoreActions.boundTo = function (Store) {
+  var actions = new this();
+
+  var functionAttrs = _(actions).functions();
+  _(functionAttrs).each(function (attr) {
+    actions[attr] = actions[attr].bind(Store);
+  });
+
+  return actions;
+};
+
+_(CrudStoreActions.prototype).extend({
+  create: function (data) {
+    var model = new this._collection.model(data);
+    this._collection.add(model);
+  },
+
+  createAndSave: function (data) {
+    this._collection.create(data);
+  },
+
+  update: function (id, data) {
+    var model = this._collection.get(id);
+    if (!model) {
+      throw 'Cannot call update action with id: ' + id + ' because model ' +
+        'not exist.';
+    }
+
+    model.set(data);
+  },
+
+  updateAndSave: function (id, data) {
+    var model = this._collection.get(id);
+    if (!model) {
+      throw 'Cannot call updateAndSave action with id: ' + id + ' because ' +
+        'model not exist.';
+    }
+
+    model.set(data);
+    model.save();
+  },
+
+  destroy: function (id) {
+    var model = this._collection.get(id);
+    if (!model) {
+      throw 'Cannot call destroy action with id: ' + id + ' because model ' +
+        'not exist.';
+    }
+
+    this._collection.remove(id);
+  },
+
+  destroyAndSave: function (id) {
+    var model = this._collection.get(id);
+    if (!model) {
+      throw 'Cannot call destroyAndSave action with id: ' + id + ' because ' +
+        'model not exist.';
+    }
+
+    model.destroy();
+  },
+
+  load: function (models) {
+    this._collection.reset(models);
+  },
+
+  fetchAll: function () {
+    if (!this._collection.url) {
+      throw 'fetchAll action requires \'url\' to be set on collection class!';
+    }
+
+    this._isFetchingAll = true;
+    this._collection.fetch({
+      silent: true,
+      success: function () {
+        this._isFetchingAll = false;
+      }.bind(this)
+    });
+  },
+
+  fetch: function (id) {
+    if (!this._collection.url) {
+      throw 'fetch action requires \'url\' to be set on collection class!';
+    }
+
+    var model = this._collection.get(id);
+    if (!model) {
+      model = new this._collection.model({ id: id });
+      this._collection.add(model);
+    }
+
+    this._isFetching[id] = true;
+    model.fetch({
+      success: function () {
+        this._isFetching[id] = false;
+      }.bind(this)
+    });
+  },
+
+  save: function (id) {
+    var model = this._collection.get(id);
+    if (!model) {
+      throw 'Cannot call save action with id: ' + id + ' because model ' +
+        'not exist.';
+    }
+
+    model.save();
+  }
+});
+
+module.exports = CrudStoreActions;
+
+},{"backbone":"backbone","underscore":"underscore"}],2:[function(require,module,exports){
+'use strict';
+
+var keyMirror = require('keymirror');
+
+var ACTIONS = ['create', 'destroy', 'update'];
+
+module.exports = {
+  instance: function (prefix) {
+    var constants = {};
+    for (var i = 0; i < ACTIONS.length; i++) {
+      constants[(prefix + '_' + ACTIONS[i]).toUpperCase()] = null;
+    }
+
+    return keyMirror(constants);
+  }
+};
+
+},{"keymirror":4}],3:[function(require,module,exports){
+'use strict';
+
 var Collection = require('backbone').Collection;
 var Immutable = require('immutable');
 var extend = Collection.extend;
@@ -151,135 +290,70 @@ _(CrudStore.prototype).extend({
 
 module.exports = CrudStore;
 
-},{"backbone":"backbone","immutable":"immutable","underscore":"underscore"}],2:[function(require,module,exports){
-'use strict';
+},{"backbone":"backbone","immutable":"immutable","underscore":"underscore"}],4:[function(require,module,exports){
+/**
+ * Copyright 2013-2014 Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 
-var extend = require('backbone').Collection.extend;
-var _ = require('underscore');
+"use strict";
 
-var CrudStoreActions = function () {
-};
-CrudStoreActions.extend = extend;
-CrudStoreActions.boundTo = function (Store) {
-  var actions = new this();
-
-  var functionAttrs = _(actions).functions();
-  _(functionAttrs).each(function (attr) {
-    actions[attr] = actions[attr].bind(Store);
-  });
-
-  return actions;
-};
-
-_(CrudStoreActions.prototype).extend({
-  create: function (data) {
-    var model = new this._collection.model(data);
-    this._collection.add(model);
-  },
-
-  createAndSave: function (data) {
-    this._collection.create(data);
-  },
-
-  update: function (id, data) {
-    var model = this._collection.get(id);
-    if (!model) {
-      throw 'Cannot call update action with id: ' + id + ' because model ' +
-        'not exist.';
-    }
-
-    model.set(data);
-  },
-
-  updateAndSave: function (id, data) {
-    var model = this._collection.get(id);
-    if (!model) {
-      throw 'Cannot call updateAndSave action with id: ' + id + ' because ' +
-        'model not exist.';
-    }
-
-    model.set(data);
-    model.save();
-  },
-
-  destroy: function (id) {
-    var model = this._collection.get(id);
-    if (!model) {
-      throw 'Cannot call destroy action with id: ' + id + ' because model ' +
-        'not exist.';
-    }
-
-    this._collection.remove(id);
-  },
-
-  destroyAndSave: function (id) {
-    var model = this._collection.get(id);
-    if (!model) {
-      throw 'Cannot call destroyAndSave action with id: ' + id + ' because ' +
-        'model not exist.';
-    }
-
-    model.destroy();
-  },
-
-  load: function (models) {
-    this._collection.reset(models);
-  },
-
-  fetchAll: function () {
-    if (!this._collection.url) {
-      throw 'fetchAll action requires \'url\' to be set on collection class!';
-    }
-
-    this._isFetchingAll = true;
-    this._collection.fetch({
-      silent: true,
-      success: function () {
-        this._isFetchingAll = false;
-      }.bind(this)
-    });
-  },
-
-  fetch: function (id) {
-    if (!this._collection.url) {
-      throw 'fetch action requires \'url\' to be set on collection class!';
-    }
-
-    var model = this._collection.get(id);
-    if (!model) {
-      model = new this._collection.model({ id: id });
-      this._collection.add(model);
-    }
-
-    this._isFetching[id] = true;
-    model.fetch({
-      success: function () {
-        this._isFetching[id] = false;
-      }.bind(this)
-    });
-  },
-
-  save: function (id) {
-    var model = this._collection.get(id);
-    if (!model) {
-      throw 'Cannot call save action with id: ' + id + ' because model ' +
-        'not exist.';
-    }
-
-    model.save();
+/**
+ * Constructs an enumeration with keys equal to their value.
+ *
+ * For example:
+ *
+ *   var COLORS = keyMirror({blue: null, red: null});
+ *   var myColor = COLORS.blue;
+ *   var isColorValid = !!COLORS[myColor];
+ *
+ * The last line could not be performed if the values of the generated enum were
+ * not equal to their keys.
+ *
+ *   Input:  {key1: val1, key2: val2}
+ *   Output: {key1: key1, key2: key2}
+ *
+ * @param {object} obj
+ * @return {object}
+ */
+var keyMirror = function(obj) {
+  var ret = {};
+  var key;
+  if (!(obj instanceof Object && !Array.isArray(obj))) {
+    throw new Error('keyMirror(...): Argument must be an object.');
   }
-});
+  for (key in obj) {
+    if (!obj.hasOwnProperty(key)) {
+      continue;
+    }
+    ret[key] = key;
+  }
+  return ret;
+};
 
-module.exports = CrudStoreActions;
+module.exports = keyMirror;
 
-},{"backbone":"backbone","underscore":"underscore"}],"flux-crud-store":[function(require,module,exports){
+},{}],"flux-crud-store":[function(require,module,exports){
 'use strict';
 
-var CrudStore = require('./crud_store');
-var CrudStoreActions = require('./crud_store_actions');
+var Store = require('./crud_store/store');
+var Actions = require('./crud_store/actions');
+var Constants = require('./crud_store/constants');
 
-module.exports = { Store: CrudStore, Actions: CrudStoreActions };
+module.exports = { Store: Store, Actions: Actions, Constants: Constants };
 
-},{"./crud_store":1,"./crud_store_actions":2}]},{},[]);
+},{"./crud_store/actions":1,"./crud_store/constants":2,"./crud_store/store":3}]},{},[]);
   return require('flux-crud-store');
 }))
